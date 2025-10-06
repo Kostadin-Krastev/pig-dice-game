4619bb761089a45b967ebef2336a3ab7cbc9f57f
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-10-06 21:51:30 +0300

    storing buttons elements into variables

--- script.js
+++ script.js
@@ -3,8 +3,13 @@
 const playerZeroScoreElement = document.getElementById('score--0');
 const playerOneScoreElement = document.getElementById('score--1');
 
-// Selecting dice image
+// Selecting dice image elements
 const diceImageElement = document.querySelector('.dice');
+
+// Selecting button elements
+const buttonNewGameElement = document.querySelector('.btn--new');
+const buttonRollDiceElement = document.querySelector('.btn-roll');
+const buttonHoldElement = document.querySelector('.btn--hold');
 
 // Starting conditions
 playerZeroScoreElement.textContent = 0;
