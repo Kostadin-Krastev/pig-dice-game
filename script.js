9c36012cd857f5ae81cc1f5933845627fd59174f
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-10-06 21:25:54 +0300

    create starting condition for the dice image element to be hidden

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 'use strict';
-// selecting elements players score
+// Selecting elements players score
 const playerZeroScoreElement = document.getElementById('score--0');
 const playerOneScoreElement = document.getElementById('score--1');
 
+// Selecting dice image
+const diceImageElement = document.querySelector('.dice');
+
+// Starting conditions
 playerZeroScoreElement.textContent = 0;
 playerOneScoreElement.textContent = 0;
+diceImageElement.classList.add('hidden');
