0fc32a3bb185789eade80ff8026fd9ad2a9a478b
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-10-06 21:14:17 +0300

    selecting the score elements of both players

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict';
+// selecting elements players score
+const playerZeroScoreElement = document.getElementById('score--0');
+const playerOneScoreElement = document.getElementById('score--1');
+
+playerZeroScoreElement.textContent = 0;
+playerOneScoreElement.textContent = 0;
